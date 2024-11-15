d9522398e9206e93861ef4210dc39ce52bcded59
Thomas Louis Nardou <pk88yte@eduvaud.ch>
2024-11-15 14:41:41 +0100

    feat(app): connect to MQTT client
    32m-DONE-#9 j'ai pris plus de temps que prévu car j'ai au début voulu lister la liste des brokers mais suite à un discussion avec M. Melly nous en avons conclu que cela ne servais à rien

--- Bit-Ruisseau/Bit-Ruisseau/Form1.cs
+++ Bit-Ruisseau/Bit-Ruisseau/Form1.cs
@@ -1,3 +1,9 @@
+using System.Net.Sockets;
+using System.Net;
+using MQTTnet;
+using System.Text;
+using System.Diagnostics;
+
 namespace Bit_Ruisseau
 {
     public partial class Form1 : Form
@@ -5,6 +11,32 @@ namespace Bit_Ruisseau
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void connectButton_Click(object sender, EventArgs e)
+        {
+            MqttClientFactory factory = new MqttClientFactory();
+
+            var mqttClient = factory.CreateMqttClient();
+
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer(this.hostBox.Text, 1883)
+                .WithCredentials(this.userBox.Text, this.passwordBox.Text)
+                .WithClientId(this.userBox.Text + Guid.NewGuid().ToString())
+                .WithCleanSession()
+                .Build();
+
+            var res = await mqttClient.ConnectAsync(options);
+
+            if (res.ResultCode == MqttClientConnectResultCode.Success)
+            {
+                Debug.WriteLine("Connected to MQTT broker successfully.");
+            }
         }
     }
 }
